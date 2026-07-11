741ef709148e1cdbf83daee14fcaa9bb513803d0
shivamg0203 <shivamg0203@gmail.com>
2026-07-12 02:37:37 +0530

    75th commit blog complete

---
+++ assets/affiliate.js
@@ -0,0 +1,34 @@
+const affiliateHTML = `
+<div class="affiliate-card">
+    <img src="/assets/images/speedometerprem.jpg" alt="GPS Speedometer Premium" class="affiliate-image">
+
+    <div class="affiliate-info">
+        <small>⭐ Recommended</small>
+        <h3>XPR3SS GPS Bicycle Speedometer</h3>
+        <p>Accurate speed & distance tracking.</p>
+
+        <a href="https://link.amazon/B0gzQdEXG"
+           target="_blank"
+           rel="nofollow sponsored noopener">
+           Check Price
+        </a>
+    </div>
+</div>
+<div class="affiliate-card">
+    <img src="/assets/images/waterproof_odometer.webp" alt="GPS Speedometer Cheaper" class="affiliate-image">
+
+    <div class="affiliate-info">
+        <small>⭐ Cheap and Reliable</small>
+        <h3>Lista 14 Waterproof Bicycle Odometer (6 Months warrenty)</h3>
+        <p>Waterproof design for all weather conditions.</p>
+        <a href="https://link.amazon/B0do5Lole"
+           target="_blank"
+           rel="nofollow sponsored noopener">
+           Check Price
+        </a>
+    </div>
+</div>
+
+`;
+
+document.getElementById("affiliate-container").innerHTML = affiliateHTML;
